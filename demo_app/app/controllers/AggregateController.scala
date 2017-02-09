7be704e53d1bffb5fe0bccae6945ea9571f8da76
niklas.leopold <leopold.niklas@gmail.com>
2017-02-09 20:18:16 +0100

    added circuit breakers

--- demo_app/app/controllers/AggregateController.scala
+++ demo_app/app/controllers/AggregateController.scala
@@ -7,6 +7,11 @@ import akka.actor.ActorSystem
 import scala.concurrent.{ ExecutionContext, Future, Promise }
 import scala.concurrent.duration._
 import java.util.concurrent.TimeoutException
+import akka.pattern.CircuitBreakerOpenException
+
+import akka.pattern.CircuitBreaker
+import akka.pattern.pipe
+import akka.actor.{ Actor, ActorLogging, ActorRef }
 
 
 import akka.util.Timeout
@@ -15,15 +20,24 @@ import akka.pattern.ask
 
 import play.api.libs.ws._
 
-class AggregateController @Inject() (ws: WSClient)(implicit exec: ExecutionContext)extends Controller{
+class AggregateController @Inject() (ws: WSClient,actorSystem: ActorSystem)(implicit exec: ExecutionContext)extends Controller{
   
-  val request1: WSRequest = ws.url("http://localhost:9000/mock-service/important").withRequestTimeout(2000.millis)
-  val request2: WSRequest = ws.url("http://localhost:9000/mock-service/not_important").withRequestTimeout(2000.millis)
+  val important: WSRequest = ws.url("http://localhost:9000/mock-service/important").withRequestTimeout(2000.millis)
+  val notImportant: WSRequest = ws.url("http://localhost:9000/mock-service/not_important").withRequestTimeout(2000.millis)
+  
+  val importantBreaker =
+    new CircuitBreaker(actorSystem.scheduler, maxFailures = 5, callTimeout = 2.seconds, resetTimeout = 1.minute).onOpen(notifyMeOnOpen("important"))
+      
+  val notImportantBreaker =
+    new CircuitBreaker(actorSystem.scheduler, maxFailures = 5, callTimeout = 2.seconds, resetTimeout = 1.minute).onOpen(notifyMeOnOpen("not_important"))      
+ 
+  def notifyMeOnOpen(source: String): Unit =
+    println(s"$source: CircuitBreaker is now open, and will not close for one minute")
   
   
   def aggregate = Action.async {
-    val r1 = request1.get().map(r => r.body)
-    val r2 = request2.get().map(r => r.body).recoverWith{
+    val r1 = importantBreaker.withCircuitBreaker(important.get().map(r => r.body))
+    val r2 = notImportantBreaker.withCircuitBreaker(notImportant.get().map(r => r.body)).recoverWith{
       case e => Future("Nothing important anyway") 
     }
     
@@ -36,6 +50,7 @@ class AggregateController @Inject() (ws: WSClient)(implicit exec: ExecutionConte
       Ok(aggregated._1 + " ; " +  aggregated._2)
     }.recoverWith {
       case e: TimeoutException => Future(RequestTimeout)
+      case e: CircuitBreakerOpenException => Future(RequestTimeout)
     }
   }
   
