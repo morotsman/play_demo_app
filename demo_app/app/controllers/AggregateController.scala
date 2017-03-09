86fffa6ea6163e0353d4970ec40ec480fcda0240
morotsman <leopold.niklas@gmail.com>
2017-03-09 16:00:39 +0100

    bug fixes

--- demo_app/app/controllers/AggregateController.scala
+++ demo_app/app/controllers/AggregateController.scala
@@ -23,21 +23,23 @@ class AggregateController @Inject() (ws: WSClient, actorSystem: ActorSystem, con
 
   val importantUrl = configuration.getString("importantUrl").get
   val importantTimeOutInMillis = configuration.getInt("importantTimeOutInMillis").get
+  val importantResetTimeOutInMillis = configuration.getInt("importantResetTimeOutInMillis").get
 
   val notImportantUrl = configuration.getString("notImportantUrl").get
   val notImportantTimeOutInMillis = configuration.getInt("notImportantTimeOutInMillis").get
+  val nonImportantResetTimeOutInMillis = configuration.getInt("nonImportantResetTimeOutInMillis").get
 
   val importantRequest: WSRequest = ws.url(importantUrl).withRequestTimeout(importantTimeOutInMillis.millis)
   val notImportantRequest: WSRequest = ws.url(notImportantUrl).withRequestTimeout(notImportantTimeOutInMillis.millis)
 
   val importantBreaker =
-    new CircuitBreaker(actorSystem.scheduler, maxFailures = 5, callTimeout = importantTimeOutInMillis.millis, resetTimeout = 1.minute)
+    new CircuitBreaker(actorSystem.scheduler, maxFailures = 5, callTimeout = importantTimeOutInMillis.millis, resetTimeout = importantResetTimeOutInMillis.millis)
       .onClose(notifyMe("important", "closed"))
       .onOpen(notifyMe("important", "open"))
       .onHalfOpen(notifyMe("important", "half open"))
 
   val notImportantBreaker =
-    new CircuitBreaker(actorSystem.scheduler, maxFailures = 5, callTimeout = notImportantTimeOutInMillis.millis, resetTimeout = 1.minute)
+    new CircuitBreaker(actorSystem.scheduler, maxFailures = 5, callTimeout = notImportantTimeOutInMillis.millis, resetTimeout = nonImportantResetTimeOutInMillis.millis)
       .onClose(notifyMe("not_important", "closed"))
       .onOpen(notifyMe("not_important", "open"))
       .onHalfOpen(notifyMe("not_important", "half open"))
@@ -45,15 +47,20 @@ class AggregateController @Inject() (ws: WSClient, actorSystem: ActorSystem, con
 
   //handles route /important
   def important = Action.async {
-    importantBreaker.withCircuitBreaker(importantRequest.get().map(r => Ok(r.body))).recoverWith(errorHandler)
+    importantBreaker.withCircuitBreaker {
+      importantRequest.get().map(mapImportantResponse)
+    }.recoverWith(errorHandler)
   }
 
   //handles route /aggregate
   def aggregate = Action.async {
-    val important = importantBreaker.withCircuitBreaker(importantRequest.get().map(r => r.body))
-    val notImportant = notImportantBreaker.withCircuitBreaker(notImportantRequest.get().map(r => r.body)).recoverWith(notImportantErrorHandler)
+    val important: Future[String] = 
+      importantBreaker.withCircuitBreaker(importantRequest.get().map(mapAggregateImportantResponse))
+    
+    val notImportant: Future[String] = 
+      notImportantBreaker.withCircuitBreaker(notImportantRequest.get().map(mapAggregateNonImportantResponse)).recoverWith(notImportantErrorHandler)
 
-    val aggregatedResponse = for (
+    val aggregatedResponse: Future[Result] = for (
       response1 <- important;
       response2 <- notImportant
     ) yield Ok(response1 + " ; " + response2)
@@ -61,9 +68,20 @@ class AggregateController @Inject() (ws: WSClient, actorSystem: ActorSystem, con
     aggregatedResponse.recoverWith(errorHandler)
   }
   
+  def mapImportantResponse(r: WSResponse) : Result = if(r.status == 200) Ok(r.body) else InternalServerError("Error when calling external system.")
+  
+  def mapAggregateNonImportantResponse(r: WSResponse) : String = 
+    if(r.status ==200) r.body else throw new RuntimeException("Error when calling non important service")
+ 
+  
+  def mapAggregateImportantResponse(r: WSResponse) : String = 
+    if(r.status ==200) r.body else throw new RuntimeException("Error when calling important service")
+ 
+  
   val errorHandler: PartialFunction[Throwable, Future[Result]] = {
     case e: TimeoutException            => Future(RequestTimeout)
     case e: CircuitBreakerOpenException => Future(RequestTimeout)
+    case _ => Future(InternalServerError) 
   }
   
   val notImportantErrorHandler: PartialFunction[Throwable, Future[String]] = {
